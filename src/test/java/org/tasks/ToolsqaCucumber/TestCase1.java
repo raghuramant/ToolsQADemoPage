eb917ef7268a38b25d98c4e8f3220f431bdbf83e
raghuramant <raghuramanth@gmail.com>
2018-06-13 09:29:39 +0530

    added test case1

---
+++ src/test/java/org/tasks/ToolsqaCucumber/TestCase1.java
@@ -0,0 +1,13 @@
+package org.tasks.ToolsqaCucumber;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testcase1();
+	}
+
+	static void testcase1() {
+		System.out.println("test case one");
+	}
+}
